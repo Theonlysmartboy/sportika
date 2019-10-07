e243d8605a9e291f813977d78a307ec6bf62a33f
TheOnlySmartBoy <o2jose43@gmail.com>
2019-10-07 12:43:26 +0300

    Completed admin layout design

--- resources/views/layouts/adminlayout/admin_footer.blade.php
+++ resources/views/layouts/adminlayout/admin_footer.blade.php
@@ -1,8 +1,5 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<!--Footer-part-->
+<div class="row-fluid">
+    <div id="footer" class="span12"> 2013 &copy; Matrix Admin. Brought to you by <a href="http://themedesigner.in">Themedesigner.in</a> </div>
+</div>
+<!--end-Footer-part-->
